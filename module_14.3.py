431575f5656e8255eeb0c370706082677cb5dfba
Safar <safqwer.136@gmail.com>
2024-11-22 00:40:10 +0300

    Update module_14.3.py

--- module_14.3.py
+++ module_14.3.py
@@ -4,7 +4,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from keyboards import *
 
-api = '7812004269:AAGnGigM_tZonbzCTlqyeef0g89MBm6xQQg'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
