525d4f98ef946254e0f611433b90e2e002875a43
Safar <safqwer.136@gmail.com>
2024-11-21 15:55:25 +0300

    Update module_14.1.py

--- module_14.1.py
+++ module_14.1.py
@@ -14,31 +14,30 @@ balance INTEGER NOT NULL
 ''')
 
 '''TASK 1'''
-# for i in range(1, 11):
-#     cursor.execute(' INSERT INTO Users(username, email, age, balance) VALUES (?, ?, ?, ?)', (f'User{i}',
-#                                                                         f'example{i}@gmail.com', f'{i * 10}', '1000'))
+for i in range(1, 11):
+    cursor.execute(' INSERT INTO Users(username, email, age, balance) VALUES (?, ?, ?, ?)', (f'User{i}',
+                                                                        f'example{i}@gmail.com', f'{i * 10}', '1000'))
 
 '''TASK 2'''
-# cursor.execute(' UPDATE Users SET balance = ? WHERE id % 2 > 0', (500, ))
+cursor.execute(' UPDATE Users SET balance = ? WHERE id % 2 > 0', (500, ))
 
 '''TASK 3'''
-# k = 1
-# while k < 11:
-#     cursor.execute('DELETE FROM Users WHERE username = ?', (f'User{k}', ))
-#     k += 3
+k = 1
+while k < 11:
+    cursor.execute('DELETE FROM Users WHERE username = ?', (f'User{k}', ))
+    k += 3
 
-cursor.execute('SELECT * FROM Users')                  # GROUP - sorting
+cursor.execute('SELECT * FROM Users WHERE age != 60')
 users = cursor.fetchall()
-for user in users:
-    print(user)
+for i in users:
+    print(f'Имя: {i[1]} | Почта: {i[2]} | Возраст: {i[3]} | Баланс: {i[4]}')
 
 connection.commit()
 connection.close()
 
 '''RUN'''
-# (2, 'User2', 'example2@gmail.com', 20, 1000)
-# (3, 'User3', 'example3@gmail.com', 30, 500)
-# (5, 'User5', 'example5@gmail.com', 50, 500)
-# (6, 'User6', 'example6@gmail.com', 60, 1000)
-# (8, 'User8', 'example8@gmail.com', 80, 1000)
-# (9, 'User9', 'example9@gmail.com', 90, 500)
+# Имя: User2 | Почта: example2@gmail.com | Возраст: 20 | Баланс: 1000
+# Имя: User3 | Почта: example3@gmail.com | Возраст: 30 | Баланс: 500
+# Имя: User5 | Почта: example5@gmail.com | Возраст: 50 | Баланс: 500
+# Имя: User8 | Почта: example8@gmail.com | Возраст: 80 | Баланс: 1000
+# Имя: User9 | Почта: example9@gmail.com | Возраст: 90 | Баланс: 500
